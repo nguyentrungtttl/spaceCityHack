34164caabfd098c9012d45aa2fd9cf4930a0dc90
User <user@192.168.102.7>
2024-07-21 17:22:15 +0700

    fixed

--- screens/Auth/login/index.js
+++ screens/Auth/login/index.js
@@ -1,45 +1,47 @@
-import {View,Text,TextInput,SafeAreaView,TouchableOpacity,Platform,Dimensions,TouchableWithoutFeedback,Keyboard,KeyboardAvoidingView,Image} from "react-native"
-import { Svg,Path } from "react-native-svg"
-import { LinearGradient } from "expo-linear-gradient"
-import { styles } from "./style"
-const screenWidth= Dimensions.get("screen").width
-const screenHeight = Dimensions.get("screen").height
-export default function Login({navigation}){
-    return(
+import React from 'react';
+import { View, Text, TextInput, SafeAreaView, TouchableOpacity, Dimensions, Image } from 'react-native';
+import { styles } from './style';
 
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
 
+export default function Login({ navigation }) {
+  return (
     <SafeAreaView style={styles.container}>
-        <Image source={require("../../../assets/waving.png")} style={styles.image}/>
+      <View style={styles.topContainer}>
+        <Image source={require('../../../assets/waving.png')} style={styles.image} />
+        <View style={styles.curvedBackground}></View>
+      </View>
 
-        <View style={[styles.box,{height:screenHeight*0.78}]}>
-            <Text style={styles.title}>Welcome back!</Text>
+      <View style={[styles.box, { height: screenHeight * 0.68 }]}>
+        <Text style={styles.title}>Welcome back!</Text>
 
-            <View style={{width:"100%",marginTop:10,alignItems:"center"}}>
-                <View style={{flexDirection:"row"}}>
-                    <Image style={styles.textinputinlineimage} source={require("../../../assets/mail.png")}/>
-                    <TextInput placeholder="Email address" placeholderTextColor={"gray"} style={styles.textinput}/>
-                </View>
+        <View style={{ width: '100%', marginTop: 10, alignItems: 'center' }}>
+          <View style={{ flexDirection: 'row' }}>
+            <Image style={styles.textinputinlineimage} source={require('../../../assets/mail.png')} />
+            <TextInput placeholder="Email address" placeholderTextColor={"gray"} style={styles.textinput} />
+          </View>
 
-                <View style={{flexDirection:"row"}}>
-                    <Image style={styles.textinputinlineimage} source={require("../../../assets/lock.png")}/>
-                    <TextInput placeholder="Password" placeholderTextColor={"gray"} style={styles.textinput}/>
-                </View>
-            </View>
-
-            <TouchableOpacity onPress={()=>navigation.navigate("Ready")} style={[styles.button,{marginTop:20}]}>
-                <Text style={styles.whiteBold}>Login</Text>
-            </TouchableOpacity>
-
-            <Text style={styles.whitetext}>Don't have an account?</Text>
-
-            <TouchableOpacity onPress={()=>navigation.navigate("SignUp")} style={styles.button}>
-                <Text style={styles.whiteBold}>Register</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity>
-                <Text style={styles.whiteBoldUnderlinedText}>Forgot Password</Text>
-            </TouchableOpacity>
+          <View style={{ flexDirection: 'row' }}>
+            <Image style={styles.textinputinlineimage} source={require('../../../assets/lock.png')} />
+            <TextInput placeholder="Password" placeholderTextColor={"gray"} style={styles.textinput} />
+          </View>
         </View>
-    </ SafeAreaView>
-    )
+
+        <TouchableOpacity onPress={() => navigation.navigate("Ready")} style={[styles.button, { marginTop: 20 }]}>
+          <Text style={styles.whiteBold}>Login</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.whitetext}>Don't have an account?</Text>
+
+        <TouchableOpacity onPress={() => navigation.navigate("SignUp")} style={styles.button}>
+          <Text style={styles.whiteBold}>Register</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.whiteBoldUnderlinedText}>Forgot Password</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
--- screens/Auth/login/style.js
+++ screens/Auth/login/style.js
@@ -1,76 +1,91 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
 
 export const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"#E65791"
+  container: {
+    flex: 1,
+    backgroundColor: '#E65791',
+  },
+  topContainer: {
+    width: screenWidth,
+    height: screenHeight * 0.3,
+    backgroundColor: '#AB3A96',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  curvedBackground: {
+    position: 'absolute',
+    bottom: 10,
+    width: screenWidth,
+    height: screenHeight * 0.5,
+    backgroundColor: '#E65791',
+    zIndex: -1,
+    borderBottomLeftRadius: screenWidth / 1.5,
+    borderBottomRightRadius: screenWidth / 1.5,
+  },
+  textinput: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    padding: 20,
+    width: '80%',
+    paddingLeft: 55,
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    textinput:{
-        marginTop:20,
-        backgroundColor:"white",
-        padding:20,
-        width:"80%",
-        paddingLeft:55,
-        borderRadius:50,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        // fontWeight:'20',
-    },
-    textinputinlineimage:{
-        position:"absolute",
-        zIndex:1,
-        top:38,
-        left:20
-    },
-    box:{
-        width:"100%",
-        zIndex:0,
-        backgroundColor:"#AB3A96",
-        alignItems:"center",
-        position:"absolute",
-        bottom:0
-    },
-    image:{
-        position:"absolute",
-        top:50,
-        alignSelf:"center"
-    },
-    button:{
-        backgroundColor:"#333333",
-        padding:13,
-        width:124,
-        alignItems:"center",
-        borderRadius:50
-    },
-    title:{
-        marginTop:30,
-        // fontWeight:"bold",
-        fontSize:40,
-        color:"white",
-        
-    },
-    whitetext:{
-        // fontWeight:300,
-        color:"white",
-        fontSize:17,
-        marginVertical:15
-    },
-    whiteBoldUnderlinedText:{
-        textDecorationLine:"underline",
-        color:"white",
-        // fontWeight:"bold",
-        fontSize:16,
-        marginTop:20
-    },
-    whiteBold:{
-        // fontWeight:"bold",
-        color:"white",
-        fontSize:17
-    }
-})
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  textinputinlineimage: {
+    position: 'absolute',
+    zIndex: 1,
+    top: 38,
+    left: 20,
+  },
+  box: {
+    width: '100%',
+    zIndex: 0,
+    backgroundColor: '#AB3A96',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+  },
+  image: {
+    position: 'absolute',
+    top: 70,
+    alignSelf: 'center',
+  },
+  button: {
+    backgroundColor: '#333333',
+    padding: 13,
+    width: 124,
+    alignItems: 'center',
+    borderRadius: 50,
+  },
+  title: {
+    marginTop: 30,
+    fontSize: 40,
+    color: 'white',
+  },
+  whitetext: {
+    color: 'white',
+    fontSize: 17,
+    marginVertical: 15,
+  },
+  whiteBoldUnderlinedText: {
+    textDecorationLine: 'underline',
+    color: 'white',
+    fontSize: 16,
+    marginTop: 20,
+  },
+  whiteBold: {
+    color: 'white',
+    fontSize: 17,
+  },
+});
